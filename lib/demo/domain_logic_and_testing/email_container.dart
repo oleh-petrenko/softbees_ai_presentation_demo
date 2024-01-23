e1fdd89e4ae46088351c38c9832c30a16430f34f
Oleh Petrenko <oleh.petrenko.dev@gmail.com>
2024-01-23 18:10:20 +0200

    small upd

--- lib/demo/domain_logic_and_testing/email_container.dart
+++ lib/demo/domain_logic_and_testing/email_container.dart
@@ -6,30 +6,13 @@
 /// AI request #2: provide ultimate list of test cases to unit test for the code above with a few examples of tests
 
 //Initial implementation
-// class EmailContainer {
-//   final String email;
-//
-//   EmailContainer({
-//     required this.email,
-//   });
-// }
-
-//Modified implementation
 class EmailContainer {
   final String email;
 
   EmailContainer({
     required this.email,
-  }) {
-    if (!_isValidEmail(email)) {
-      throw const FormatException('Invalid email format');
-    }
-  }
-
-  static bool _isValidEmail(String email) {
-    final emailRegex = RegExp(
-      r'^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
-    );
-    return emailRegex.hasMatch(email);
-  }
+  });
 }
+
+//Modified implementation
+
